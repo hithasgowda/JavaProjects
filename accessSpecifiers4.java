0bc68ede1beed780a28e3b642e3582d8488320b9
hithasgowda <97660180+hithasgowda@users.noreply.github.com>
2022-01-16 23:25:44 +0530

    Add files via upload

--- accessSpecifiers4.java
+++ accessSpecifiers4.java
@@ -1,11 +1,15 @@
-package pack2;
 
-public class accessSpecifiers4{
+//create another package
+package pack4;
+import pack2.*;
 
-		public static void main(String[] args) {
-			
-			pubaccessspecifiers obj = new pubaccessspecifiers(); 
-	        obj.display();  
-			
-		}
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
 	}
+}
+	
